2faad55645e4308f2b33f9b50796cdc442003175
Mensah.18154 <Mensah.18154@LABI1-6PC27>
2022-10-10 10:41:46 +0200

    Aggiungere i file di progetto.

---
+++ TELECOMANDO/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace TELECOMANDO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pulsante1 = new System.Windows.Forms.Button();
+            this.pulsante2 = new System.Windows.Forms.Button();
+            this.pulsante3 = new System.Windows.Forms.Button();
+            this.pulsante4 = new System.Windows.Forms.Button();
+            this.pulsante5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pulsante7 = new System.Windows.Forms.Button();
+            this.pulsante8 = new System.Windows.Forms.Button();
+            this.pulsante9 = new System.Windows.Forms.Button();
+            this.pulsante0 = new System.Windows.Forms.Button();
+            this.Accensione = new System.Windows.Forms.Button();
+            this.pulsantePiu = new System.Windows.Forms.Button();
+            this.pulsanteMeno = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(370, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 342);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pulsante1
+            // 
+            this.pulsante1.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls1;
+            this.pulsante1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante1.Location = new System.Drawing.Point(382, 133);
+            this.pulsante1.Name = "pulsante1";
+            this.pulsante1.Size = new System.Drawing.Size(37, 37);
+            this.pulsante1.TabIndex = 1;
+            this.pulsante1.UseVisualStyleBackColor = true;
+            // 
+            // pulsante2
+            // 
+            this.pulsante2.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls2;
+            this.pulsante2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante2.Location = new System.Drawing.Point(443, 133);
+            this.pulsante2.Name = "pulsante2";
+            this.pulsante2.Size = new System.Drawing.Size(37, 37);
+            this.pulsante2.TabIndex = 2;
+            this.pulsante2.UseVisualStyleBackColor = true;
+            // 
+            // pulsante3
+            // 
+            this.pulsante3.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls3;
+            this.pulsante3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante3.Location = new System.Drawing.Point(503, 133);
+            this.pulsante3.Name = "pulsante3";
+            this.pulsante3.Size = new System.Drawing.Size(37, 37);
+            this.pulsante3.TabIndex = 3;
+            this.pulsante3.UseVisualStyleBackColor = true;
+            // 
+            // pulsante4
+            // 
+            this.pulsante4.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls4;
+            this.pulsante4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante4.Location = new System.Drawing.Point(382, 199);
+            this.pulsante4.Name = "pulsante4";
+            this.pulsante4.Size = new System.Drawing.Size(37, 37);
+            this.pulsante4.TabIndex = 4;
+            this.pulsante4.UseVisualStyleBackColor = true;
+            // 
+            // pulsante5
+            // 
+            this.pulsante5.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls5;
+            this.pulsante5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante5.Location = new System.Drawing.Point(443, 199);
+            this.pulsante5.Name = "pulsante5";
+            this.pulsante5.Size = new System.Drawing.Size(37, 37);
+            this.pulsante5.TabIndex = 5;
+            this.pulsante5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls6;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Location = new System.Drawing.Point(503, 199);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(37, 37);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // pulsante7
+            // 
+            this.pulsante7.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls7;
+            this.pulsante7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante7.Location = new System.Drawing.Point(382, 267);
+            this.pulsante7.Name = "pulsante7";
+            this.pulsante7.Size = new System.Drawing.Size(37, 37);
+            this.pulsante7.TabIndex = 7;
+            this.pulsante7.UseVisualStyleBackColor = true;
+            // 
+            // pulsante8
+            // 
+            this.pulsante8.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls8;
+            this.pulsante8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante8.Location = new System.Drawing.Point(443, 267);
+            this.pulsante8.Name = "pulsante8";
+            this.pulsante8.Size = new System.Drawing.Size(37, 37);
+            this.pulsante8.TabIndex = 8;
+            this.pulsante8.UseVisualStyleBackColor = true;
+            // 
+            // pulsante9
+            // 
+            this.pulsante9.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls9;
+            this.pulsante9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante9.Location = new System.Drawing.Point(503, 267);
+            this.pulsante9.Name = "pulsante9";
+            this.pulsante9.Size = new System.Drawing.Size(37, 37);
+            this.pulsante9.TabIndex = 9;
+            this.pulsante9.UseVisualStyleBackColor = true;
+            // 
+            // pulsante0
+            // 
+            this.pulsante0.BackgroundImage = global::TELECOMANDO.Properties.Resources.puls0;
+            this.pulsante0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsante0.Location = new System.Drawing.Point(443, 326);
+            this.pulsante0.Name = "pulsante0";
+            this.pulsante0.Size = new System.Drawing.Size(37, 37);
+            this.pulsante0.TabIndex = 10;
+            this.pulsante0.UseVisualStyleBackColor = true;
+            // 
+            // Accensione
+            // 
+            this.Accensione.BackgroundImage = global::TELECOMANDO.Properties.Resources.tastoAccensione;
+            this.Accensione.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Accensione.Location = new System.Drawing.Point(382, 80);
+            this.Accensione.Name = "Accensione";
+            this.Accensione.Size = new System.Drawing.Size(37, 37);
+            this.Accensione.TabIndex = 11;
+            this.Accensione.UseVisualStyleBackColor = true;
+            // 
+            // pulsantePiu
+            // 
+            this.pulsantePiu.BackgroundImage = global::TELECOMANDO.Properties.Resources.pulsP;
+            this.pulsantePiu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsantePiu.Location = new System.Drawing.Point(382, 364);
+            this.pulsantePiu.Name = "pulsantePiu";
+            this.pulsantePiu.Size = new System.Drawing.Size(37, 37);
+            this.pulsantePiu.TabIndex = 12;
+            this.pulsantePiu.UseVisualStyleBackColor = true;
+            // 
+            // pulsanteMeno
+            // 
+            this.pulsanteMeno.BackgroundImage = global::TELECOMANDO.Properties.Resources.pulsM;
+            this.pulsanteMeno.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pulsanteMeno.Location = new System.Drawing.Point(503, 364);
+            this.pulsanteMeno.Name = "pulsanteMeno";
+            this.pulsanteMeno.Size = new System.Drawing.Size(37, 37);
+            this.pulsanteMeno.TabIndex = 13;
+            this.pulsanteMeno.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pulsanteMeno);
+            this.Controls.Add(this.pulsantePiu);
+            this.Controls.Add(this.Accensione);
+            this.Controls.Add(this.pulsante0);
+            this.Controls.Add(this.pulsante9);
+            this.Controls.Add(this.pulsante8);
+            this.Controls.Add(this.pulsante7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pulsante5);
+            this.Controls.Add(this.pulsante4);
+            this.Controls.Add(this.pulsante3);
+            this.Controls.Add(this.pulsante2);
+            this.Controls.Add(this.pulsante1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button pulsante1;
+        private Button pulsante2;
+        private Button pulsante3;
+        private Button pulsante4;
+        private Button pulsante5;
+        private Button button6;
+        private Button pulsante7;
+        private Button pulsante8;
+        private Button pulsante9;
+        private Button pulsante0;
+        private Button Accensione;
+        private Button pulsantePiu;
+        private Button pulsanteMeno;
+    }
+}
---
+++ TELECOMANDO/tele.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TELECOMANDO
+{
+    internal class tele
+    {
+        private string produttori = "";
+        private string modello = "";
+        private string operationMode = "";
+        private bool stato;
+        private int canale;
+        private int volume;
+
+        public tele(string p, string m, string oM)
+        {
+            produttori = p;
+            modello = m;
+            operationMode = oM;
+        }
+        public tele(string p, string m)
+        {
+            produttori = p;
+            modello = m;
+        }
+        public tele(string p)
+        {
+            produttori = p;
+        }
+
+        public void Accendi()
+        {
+            stato = true;
+        }
+        public void Spegni()
+        {
+            stato = false;
+        }
+        public void setVolume(string v)
+        {
+            volume = 0;
+            if (v == "+")
+            {
+                volume = volume + 1;
+            }
+            else if (v == "-")
+            {
+                volume = volume - 1;
+            }
+            if (volume < 0)
+            {
+                volume = 0;
+            }
+            if (volume > 100)
+            {
+                volume = 100;
+            }
+        }
+        public void setCanale(int c)
+        {
+            canale = c;
+        }
+        public int getVolume()
+        {
+            return volume;
+        }
+    }
+}
